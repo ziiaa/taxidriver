a5729dc821bad8e653139644b8293f26d88176ce
ziiaa <azzizahnur01@amikom.ac.id>
2022-03-23 22:37:13 +0700

    first commit

---
+++ taxidriver/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxidriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "Brigh";
+            taxi.OnDuty = "Yes";
+            taxi.NumPassenger = "10";
+
+            taxi.Registrasi();
+            taxi.Driver();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ taxidriver/Taxi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxidriver
+{
+    public class Taxi
+    {
+        // Properties
+        public string DriverName { get; set; }
+        public string OnDuty { get; set; }
+        public string NumPassenger { get; set; }
+
+        // Method
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama Driver : {0}", DriverName);
+            Console.WriteLine("Pick Up Passenger : {0}", OnDuty);
+            Console.WriteLine("Drop Off Passenger : {0}", NumPassenger);
+            Console.WriteLine("{0} Sedang Menjemput Penumpang\n",DriverName);
+        }
+        public void Driver()
+        {
+            Console.WriteLine("{0} Selesai Mengantar Penumpang\n", DriverName);
+        }
+    }
+}
